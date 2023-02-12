7e19f8a5c2c000ddfc8521b414908460f267f90a
Yaroslav Skirlyak <skirlyak2004@mail.ru>
2023-02-12 18:58:02 +0300

    os memory usage

--- 3/1.cpp
+++ 3/1.cpp
@@ -5,32 +5,60 @@
 #include <vector>
 #include <iomanip>
 #include <sys/time.h>
-#include <mach/task.h>
-#include <mach/mach_init.h>
 #include <cmath>
 
 using namespace std;
+
+// ---------- macos ----------
+
+// #include <mach/task.h>
+// #include <mach/mach_init.h>
+
+// void getMemoryUsage() { 
+// 	struct task_basic_info t_info;
+// 	mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+// 	if (KERN_SUCCESS != task_info(mach_task_self(),
+// 		TASK_BASIC_INFO, (task_info_t)&t_info,
+// 		&t_info_count))
+// 	{
+// 		printf("Error!");
+// 	}
+// 	else
+// 	{
+// 		printf("Memory used: %f MB\n", t_info.resident_size / 1024.0 / 1024.0);
+// 	}
+// }
+
+// ---------- windows ----------
+
+// #include "windows.h"
+// #include "psapi.h"
+
+// void printMemoryUse() {
+// 	PROCESS_MEMORY_COUNTERS_EX pmc;
+// 	GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+// 	SIZE_T virtualMemUsedByMe = pmc.PrivateUsage;
+
+// 	cout << fixed << setprecision(6);
+// 	cout << "Memory used: " << double(virtualMemUsedByMe) / (1024. * 1024) << " MB\n";
+// }
+
+// ---------- linux ----------
+
+#include <sys/resource.h>
+#include <sys/time.h>
+
+void printMemoryUsage() {
+   struct rusage usage;
+	  getrusage(RUSAGE_SELF, &usage);
+	  cout << "Memory used: " << usage.ru_maxrss / 1024.0 << " MB\n";
+}
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
 clock_t start;
-
-void getMemoryUsage() {
-	struct task_basic_info t_info;
-	mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
-
-	if (KERN_SUCCESS != task_info(mach_task_self(),
-		TASK_BASIC_INFO, (task_info_t)&t_info,
-		&t_info_count))
-	{
-		printf("Error!");
-	}
-	else
-	{
-		printf("Memory used: %f MB\n", t_info.resident_size / 1024.0 / 1024.0);
-	}
-}
 
 void getFirstTime() {
 	start = clock();
@@ -69,6 +97,6 @@ int main() {
     }
     fout << sum << endl;
     printTimeUse();
-    getMemoryUsage();
+    printMemoryUsage();
     return 0;
 }
