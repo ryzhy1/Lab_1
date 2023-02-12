7512e81f4d0f6d885e6fabcd538a13e0a9df439f
Skirliak Iaroslav <57949970+ryzhy1@users.noreply.github.com>
2023-02-12 08:25:37 -0500

    Add files via upload

---
+++ 2/1.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <sys/time.h>
+#include <mach/task.h>
+#include <mach/mach_init.h>
+
+using namespace std;
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+clock_t start;
+
+void getMemoryUsage() {
+	struct task_basic_info t_info;
+	mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
+
+	if (KERN_SUCCESS != task_info(mach_task_self(),
+		TASK_BASIC_INFO, (task_info_t)&t_info,
+		&t_info_count))
+	{
+		printf("Error!");
+	}
+	else
+	{
+		printf("Memory used: %f MB\n", t_info.resident_size / 1024.0 / 1024.0);
+	}
+}
+
+void getFirstTime() {
+	start = clock();
+}
+
+void printTimeUse() {
+	printf("Time taken: %.7fs\n", (double)(clock() - start)/CLOCKS_PER_SEC);
+}
+
+int main() {
+	getFirstTime();
+
+	int d, m, n;
+	fin >> d >> m >> n;
+	vector<int> stops(n);
+	for (int i = 0; i < n; i++) {
+		fin >> stops[i];
+	}
+
+	stops.push_back(d);
+	sort(stops.begin(), stops.end());
+	int currentStop = 0;
+	int currentFuel = m;
+	int result = 0;
+	for (int i = 0; i < stops.size(); i++) {
+		if (stops[i] - currentStop > currentFuel) {
+			if (stops[i] - stops[i-1] > m) {
+				result = -1;
+				break;
+			}
+			currentStop = stops[i-1];
+			currentFuel = m;
+			result++;
+		}
+		currentFuel -= stops[i] - currentStop;
+		currentStop = stops[i];
+	}
+	fout << result;
+	printTimeUse();
+	getMemoryUsage();
+	return 0;
+}
