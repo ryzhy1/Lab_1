800b3876802be0fb66d1f12f718fbb32a8ecc6d9
ryzhy1 <skirlyak2004@mail.ru>
2023-02-26 14:47:43 +0300

    11

---
+++ 11/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+ifstream fin("input.txt");
+ofstream fout("output.txt");
+
+int main() {
+    int W, n;
+    fin >> W >> n;
+
+    int dp[n+1][W+1];
+
+    for (int i = 0; i < n; i++) {
+        
+    }
+}
